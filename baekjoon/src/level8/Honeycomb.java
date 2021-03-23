c804a36f6606fab03fbbebabdf6bff5deb2611eb
JeongEun <lisasje25@gmail.com>
2021-03-24 01:15:28 +0900

    0324

---
+++ baekjoon/src/level8/Honeycomb.java
@@ -0,0 +1,29 @@
+package level8;
+
+import java.util.*;
+
+public class Honeycomb {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		int count=1;
+		int start=2;
+		
+		
+		if(n==1) {
+			System.out.println(1);
+		}
+		else {
+			while(start<=n) {
+				start+=count*6;
+				count++;
+			}
+			System.out.println(count);
+		}
+		sc.close();
+	}
+
+}
