c1d063be1a3bf4c67d62b079413b1a6f1f778dec
JeongEun <lisasje25@gmail.com>
2021-03-25 23:18:12 +0900

    0325

---
+++ baekjoon/src/level9/EscapeARectangle.java
@@ -0,0 +1,22 @@
+package level9;
+
+import java.util.Scanner;
+
+public class EscapeARectangle {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int x = sc.nextInt();
+		int y = sc.nextInt();
+		int w = sc.nextInt();
+		int h = sc.nextInt();
+        
+		int xmin = Math.min(x, w-x);	// x축 최소거리
+		int ymin = Math.min(y, h-y);	// y축 최소거리
+        
+		// x와 y축 최소거리 중 가장 작은 값
+		System.out.println(Math.min(xmin, ymin));
+	}
+
+}
