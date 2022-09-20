b86d489ae67cf36664a9ecf3bb9a99f94de413ae
SinJeongEun <lisasje25@gmail.com>
2022-09-20 16:35:55 +0900

    최소한으로 카드 정렬 1715

---
+++ src/main/java/com/baekjoon/greedy/SortCards_1715.java
@@ -0,0 +1,40 @@
+package com.example.baekjoon.baekjoon.greedy;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+import java.util.function.Function;
+
+public class SortCards_1715 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Function<String, Integer> func = Integer::parseInt;
+
+        int n = func.apply(br.readLine());
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+        for (int i = 0; i < n; i++) {
+            minHeap.add(func.apply(br.readLine()));
+        }
+
+        if(n == 1) {
+            System.out.println(0);
+            return;
+        }
+
+        int total = 0;
+        while (!minHeap.isEmpty()) {
+            int together = 0;
+            int a = minHeap.poll();
+            int b = minHeap.poll();
+            total += a + b;
+            together = a + b;
+
+            if(minHeap.size() > 0) {
+                minHeap.add(together);
+            }
+        }
+
+        System.out.println(total);
+    }
+}
