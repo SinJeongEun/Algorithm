298794c8388de987a1f0e5ac03e9a2e95a642c56
SinJeongEun <lisasje25@gmail.com>
2024-06-12 22:37:14 +0900

    프로그래머스 49190 풀이 중

---
+++ src/main/java/com/baekjoon/graph/CountRooms_programmers_49190.java
@@ -0,0 +1,22 @@
+package com.example.baekjoon.baekjoon.graph;
+
+public class CountRooms_programmers_49190 {
+    public static void main(String[] args) {
+
+    }
+
+    public int solution(int[] arrows) {
+        int answer = 0;
+
+        // todo [2, 5, 2, 7] 처럼 교차점이 생기는 경우 처리 필요함
+    /**
+     *       1 --- 2
+     *        \   /
+     *           /             => 이 경우 간선이 교차되어 방이 2개임
+     *        /    \
+     *       /      \
+     *      3 --------4
+     */
+        return answer;
+    }
+}
