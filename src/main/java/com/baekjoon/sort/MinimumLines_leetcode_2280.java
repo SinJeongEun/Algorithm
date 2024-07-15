dbdc7fb5f1cea5dd619c9a8a98a2648322189d36
SinJeongEun <lisasje25@gmail.com>
2024-07-15 22:55:11 +0900

    leetcode 2280 기울기 수정 완료

--- src/main/java/com/baekjoon/sort/MinimumLines_leetcode_2280.java
+++ src/main/java/com/baekjoon/sort/MinimumLines_leetcode_2280.java
@@ -7,15 +7,14 @@ import java.util.Comparator;
 public class MinimumLines_leetcode_2280 {
     public static void main(String[] args) {
 //        int[][] stockPrices = {{1,7},{2,6},{3,5},{4,4},{5,4},{6,3},{7,2},{8,1}};
-//        int[][] stockPrices = {{3,4},{1,2},{7,8},{2,3}};
+        int[][] stockPrices = {{3,4},{1,2},{7,8},{2,3}};
 //        int[][] stockPrices = {{36,9},{17,93},{34,4},{30,11},{11,41},{53,36},{5,92},{81,92},{28,36},{3,45},{72,33},{64,1},{4,70},{16,73},{99,20},{49,33},{47,74},{83,91}};
 //        int[][] stockPrices = {{83,35},{79,51},{61,48},{54,87},{44,93},{22,5},{87,28},{64,8},{89,78},{62,83},{58,72},{48,7},{97,16},{27,100},{65,48},{11,31},{29,76},{93,29},{72,59},{73,74},{9,90},{66,81},{12,8},{86,80},{84,43},{36,63},{80,45},{81,88},{95,5},{40,59}};
 //        int[][] stockPrices = {{52,62},{12,54},{84,51},{90,48},{88,82},{26,68},{98,24},{74,92},{44,65},{72,16},{21,21},{32,74},{94,28},{27,7},{76,94},{87,81},{51,45},{66,17},{86,99},{14,75},{68,6},{46,47},{89,14}};
-        int[][] stockPrices = {{1, 1},{499999999, 2},{999999998, 3},{1000000000, 5}};
+//        int[][] stockPrices = {{1, 1},{499999999, 2},{999999998, 3},{1000000000, 5}};
 
         int result = minimumLines(stockPrices);
         System.out.println(result);
-
     }
 
     public static int minimumLines(int[][] stockPrices) {
@@ -31,17 +30,17 @@ public class MinimumLines_leetcode_2280 {
 
         Collections.sort(stockList, Comparator.comparing(Stock::getDay));
 
-        int diff_a = (stockList.get(1).price - stockList.get(0).price) / ((stockList.get(1).day - stockList.get(0).day));
-        double diff_b = (stockList.get(1).price - stockList.get(0).price) % (stockList.get(1).day - stockList.get(0).day);
+        int diff_a = stockList.get(1).price - stockList.get(0).price;
+        int diff_b = stockList.get(1).day - stockList.get(0).day;
 
         for (int i = 2; i < stockList.size(); i++) {
             Stock prev = stockList.get(i - 1);
             Stock now = stockList.get(i);
 
-            int tmp_a = (now.price - prev.price) / (now.day - prev.day);
-            double tmp_b = (now.price - prev.price) % (now.day - prev.day);
+            int tmp_a = now.price - prev.price;
+            int tmp_b = now.day - prev.day;
 
-            if(diff_a == tmp_a && diff_b == tmp_b) {
+            if((diff_a == tmp_a && diff_b == tmp_b) || (diff_a * tmp_b == diff_b * tmp_a)) {
                 continue;
             }
             lines++;
